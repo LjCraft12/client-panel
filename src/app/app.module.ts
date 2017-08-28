2762ca1b8ac862d45d1399dde2c33d656899fcfc
Perry Craft <LjCraft12@gmail.com>
2017-08-28 12:36:16 -0400

    register component works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,14 +26,16 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 
 // Service Imports
 import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'add-client', component: AddClientComponent},
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'edit-client/:id', component: EditClientComponent}
+  {path: 'add-client', component: AddClientComponent, canActivate: [AuthGuard]},
+  {path: 'client/:id', component: ClientDetailsComponent, canActivate: [AuthGuard]},
+  {path: 'edit-client/:id', component: EditClientComponent, canActivate: [AuthGuard]}
 ];
 
 export const firebaseConfig = {
@@ -69,7 +71,9 @@ export const firebaseConfig = {
   providers: [
     AngularFireAuth,
     AngularFireDatabase,
-    ClientService
+    ClientService,
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
